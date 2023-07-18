93bd68711da2879ed5acf1285022e62bf08f0d4f
Vinicius <sydoafk@gmail.com>
2023-07-18 00:53:35 -0300

    fix: update signin example with new builder feature

--- examples/signin.rs
+++ examples/signin.rs
@@ -1,4 +1,4 @@
-use iqoption_rs::IQOptionClient;
+use iqoption_rs::IQOptionBuilder;
 
 // A simple type alias so as to DRY.
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
@@ -10,11 +10,19 @@ async fn main() -> Result<()> {
 
     // FIXME: Incomplete example
 
-    let client = IQOptionClient::default()
+    let client = IQOptionBuilder::default()
         .identification(email)
         .password(password)
-        .connect()
+        .build()
         .await;
+
+    if client.is_err() {
+        panic!("Failed to connect to IQOption API");
+    }
+
+    // let client = client.unwrap();
+
+    println!("Connected to IQOption API");
 
     Ok(())
 }
