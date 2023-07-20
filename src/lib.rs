2e78ffaa4b63b59edda0cad1fb60af1138502bac
Vinicius <sydoafk@gmail.com>
2023-07-19 21:12:19 -0300

    refactor: rename dto folder to dtos

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 mod builder;
 mod client;
 mod credentials;
-mod dto;
+mod dtos;
 mod enviroment;
 mod session;
 
 pub use builder::IQOptionBuilder;
 pub use client::IQOptionClient;
 pub use credentials::Credentials;
-pub use dto::*;
+pub use dtos::*;
 pub use enviroment::Enviroment;
 pub use session::Session;
