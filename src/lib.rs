0cee429997269e16d8db075bd29089cb9d6ee2ac
Vinicius <sydoafk@gmail.com>
2023-07-19 21:09:29 -0300

    feat: init login dtos

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod builder;
 mod client;
 mod credentials;
+mod dto;
 mod enviroment;
 mod session;
 
 pub use builder::IQOptionBuilder;
 pub use client::IQOptionClient;
 pub use credentials::Credentials;
+pub use dto::*;
 pub use enviroment::Enviroment;
 pub use session::Session;
